d69c5cc425662369c96d5f34a170f7768535f8cc
tbricca <tbricca@gmail.com>
2018-07-25 13:42:43 -0700

    removed all the extra add ons that are given when installing react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  //renders the main app component
+  // Puts it in an element with the id of root
+  <App />,
+  document.getElementById("root")
+);
 registerServiceWorker();
