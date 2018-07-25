2472217fce49357ed013f65a9eaa0894419f3ec9
tbricca <tbricca@gmail.com>
2018-07-25 14:59:10 -0700

    installed axios and a bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Hello
-      </div>
-    );
+    return <div className="App">Hello</div>;
   }
 }
 
