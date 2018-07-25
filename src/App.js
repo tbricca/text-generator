b159c9f1811c8fca1947bf268e870263db9f5bcc
tbricca <tbricca@gmail.com>
2018-07-25 16:50:11 -0700

    added new files and specically a new folder to handle the select component for the user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Output from "./Components/Output";
 import axios from "axios";
 
 class App extends Component {
@@ -40,7 +41,11 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <Output value={this.state.text} />
+      </div>
+    );
   }
 }
 
---
+++ src/Components/Output.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Output extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //whatever is passed into as a property is going to go into the stae value
+      value: props.value
+    };
+  }
+
+  render() {
+    return <div className="output">{this.props.value}</div>;
+  }
+}
+
+export default Output;
