f1936cf937e09d188ab6585a4a85dd9a9a6dc032
tbricca <tbricca@gmail.com>
2018-07-27 11:20:46 -0700

    added in feature to modify the pparagraph length

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Output from "./Components/Output";
 import Select from "./Components/Controls/Select";
+import Text from "./Components/Controls/Text";
 import axios from "axios";
 
 class App extends Component {
@@ -45,13 +46,24 @@ class App extends Component {
     this.setState({ html: x }, this.getSampleText);
   }
 
+  changeParas(number) {
+    this.setState({ paras: number }, this.getSampleText);
+  }
+
   render() {
     return (
       <div className="App Container">
         <h1>ReactJS Sample Text Generator</h1>
         <hr />
-        <form class="form-inline">
-          <div class="form-group">
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text
+              value={this.state.paras}
+              onChange={this.changeParas.bind(this)}
+            />
+          </div>
+          <div className="form-group">
             <label>Include HTML:</label>
             <Select
               value={this.state.html}
